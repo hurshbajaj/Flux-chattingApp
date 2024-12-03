d5fef689d18aad956fbc2ef794813c0170260bc2
Hursh Bajaj <hurshbajaj2011@gmail.com>
2024-12-03 13:36:32 +0530

    tailwindcss init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primaryBg: "#90E0EF"
+      }
+    },
+  },
+  plugins: [],
+}
+
